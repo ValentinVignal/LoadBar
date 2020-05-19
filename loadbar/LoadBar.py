fd06ad0d2ca6ae95814d8579d814972929fb9484
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-05-20 00:17:53 +0800

    Can show percentage

--- loadbar/LoadBar.py
+++ loadbar/LoadBar.py
@@ -1,9 +1,13 @@
+import math
+
+
 class LoadBar:
     """
 
     """
 
-    def __init__(self, max=100, size=20, head='.', body='.', border_left='[', border_right=']'):
+    def __init__(self, max=100, size=20, head='.', body='.', border_left='[', border_right=']', show_step=True,
+                 show_percentage=True):
         """
 
         :param max: int: Max value of the load
@@ -14,8 +18,10 @@ class LoadBar:
         self.body = body
         self.border_left = border_left
         self.border_right = border_right
+        self.show_step = show_step
+        self.show_percentage = show_percentage
 
-        self.i = 0      # State of the progress
+        self.i = 0  # State of the progress
 
     def start(self):
         """
@@ -34,8 +40,11 @@ class LoadBar:
             self.i += 1 if to_add is None else to_add
         else:
             self.i = step
-        s = ''
-        s += self._get_bar()
+        l = list()
+        if self.show_step: l.append(self.get_step())
+        if self.show_percentage: l.append(self.get_percentage())
+        l.append(self._get_bar())
+        s = ' '.join(l)
         self._print(s, end=end)
 
     def end(self):
@@ -56,7 +65,22 @@ class LoadBar:
         done = int(min(self.i, self.max) * self.size // self.max)
         todo = self.size - done
 
-        todo_head = min(todo, 1)        # 1 or 0
+        todo_head = min(todo, 1)  # 1 or 0
         todo_blank = todo - todo_head
         return f'{self.border_left}{self.body * done}{self.head * todo_head}{" " * todo_blank}{self.border_right}'
+
+    def get_step(self):
+        if not self.show_step:
+            return ''
+        digit_nb = int(1 + math.ceil(math.log10(self.max)))
+        return '{0:{1}}'.format(self.i, digit_nb) + f'/{self.max}'
+
+    def get_percentage(self):
+        if not self.show_percentage:
+            return ''
+        percentage = self.i * 100 / self.max
+        percentage_string = f'{percentage:3.0f}%'
+        if self.show_step:
+            percentage_string = f'({percentage_string})'
+        return percentage_string
 
